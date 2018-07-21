012cf30c21656d6d9ca9b7117ffda8014d7b8be7
Sergey Seroukhov <seroukhov@Sergeys-MacBook-Pro.local>
2018-07-21 03:41:18 -0700

    Initial commit

---
+++ environment.ps1
@@ -0,0 +1,28 @@
+$global:All = @(
+    "pip-services-commons-node"
+    "pip-services-components-node"
+    "pip-services-container-node"
+    "pip-services-test-node"
+    "pip-services-data-node"
+    "pip-services-rpc-node"
+    "pip-services-messaging-node"
+    "pip-services-facade-node"
+    "pip-services-elasticsearch-node"
+    "pip-services-memcached-node"
+    "pip-services-redis-node"
+    "pip-services-fluentd-node"
+    "pip-services-mongodb-node"
+    "pip-services-prometheus-node"
+    "pip-services-aws-node"
+)
+
+$global:Frameworks = @(
+    "pip-services-commons-node"
+    "pip-services-components-node"
+    "pip-services-test-node"
+    "pip-services-container-node"
+    "pip-services-data-node"
+    "pip-services-rpc-node"
+    "pip-services-messaging-node"
+    "pip-services-facade-node"
+)
---
+++ workspace.conf.ps1
@@ -0,0 +1,20 @@
+$VersionControl = 'git'
+$VersionControlRepos = @(
+	"https://github.com/pip-services/pip-services-devenv.git"
+
+	"https://github.com/pip-services-node/pip-services-commons-node.git"
+	"https://github.com/pip-services-node/pip-services-components-node.git"
+	"https://github.com/pip-services-node/pip-services-container-node.git"	
+	"https://github.com/pip-services-node/pip-services-test-node.git"	
+	"https://github.com/pip-services-node/pip-services-data-node.git"	
+	"https://github.com/pip-services-node/pip-services-rpc-node.git"	
+	"https://github.com/pip-services-node/pip-services-messaging-node.git"	
+	"https://github.com/pip-services-node/pip-services-facade-node.git"	
+	"https://github.com/pip-services-node/pip-services-elasticsearch-node.git"	
+	"https://github.com/pip-services-node/pip-services-fluentd-node.git"	
+	"https://github.com/pip-services-node/pip-services-memcached-node.git"	
+	"https://github.com/pip-services-node/pip-services-redis-node.git"	
+	"https://github.com/pip-services-node/pip-services-mongodb-node.git"	
+	"https://github.com/pip-services-node/pip-services-prometheus-node.git"	
+	"https://github.com/pip-services-node/pip-services-aws-node.git"	
+)
